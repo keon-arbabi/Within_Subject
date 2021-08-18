4f16a25f2c641794bf2dc280ca4fe185c87335e1
Keon <keon.arbabi@gmail.com>
2021-08-18 14:23:55 -0400

    added class code

--- keon_analyses/sc-bulk-comparison-class.R
+++ keon_analyses/sc-bulk-comparison-class.R
@@ -30,6 +30,13 @@ within_meta = pheno.ws %>%
 all_meta = phenoAll %>%
   rownames_to_column(var = "sample_id")
 
+load("rawdata/MRI_Values.RData")
+
+mri = mriT %>% t() %>% as.data.frame() %>% rownames_to_column(var = "UT.code")%>%
+  inner_join(within_meta, by = "UT.code")
+mri = mri[,c(2,5)]
+names(mri)[1] = "MRI.thickness"
+
 # select 
 data_working = all_exp
 data_meta = all_meta
@@ -41,42 +48,16 @@ data_sme = Lateral_resected %>%
   rownames_to_column(var = "UT.code") 
 
 # Load in marker genes 
-marker_data = read.csv("https://raw.githubusercontent.com/sonnyc247/MarkerSelection/master/Data/Outputs/CSVs_and_Tables/Markers/All_hodge_regions/new_ALLReg_results_ITexpand_WL35IT_lfct15_minpct25_dup.csv")[,-1]
-#marker_data = read.csv("https://raw.githubusercontent.com/sonnyc247/MarkerSelection/master/Data/Outputs/CSVs_and_Tables/Markers/MTG/new_MTG_results.csv")[,-1]
-
-# Manually add inhibitory/excitatory suffix to subclass labels 
-marker_data %<>%
-  mutate(class = case_when(
-    subclass == "LAMP5" ~ "Inh",
-    subclass == "L5 ET" ~ "Exc",
-    subclass == "PAX6" ~ "Inh",
-    subclass == "L5/6 IT Car3" ~ "Exc",
-    subclass == "VIP" ~ "Inh",
-    subclass == "L6 CT" ~ "Exc",
-    subclass == "L6 IT"  ~ "Exc",
-    subclass == "L6b" ~ "Exc",
-    subclass == "PVALB" ~ "Inh",
-    subclass == "L5/6 NP" ~ "Exc",
-    subclass == "SST" ~ "Inh",
-    subclass == "L4 IT" ~ "Exc",
-    subclass == "L3/5 IT" ~ "Exc",
-    subclass == "L2/3 IT" ~ "Exc")
-    ) %>% 
-  relocate(class, .before = "subclass") %>%
-  unite(subclass, c(class, subclass), sep = "_", remove = F, na.rm = T)
-marker_data$subclass = gsub(" ", "_", marker_data$subclass)
-marker_data$class[is.na(marker_data$class)] = "NonN"
-# fix labels 
-marker_data$subclass = gsub("\\.", "/", marker_data$subclass)
+marker_data = read.csv("https://raw.githubusercontent.com/sonnyc247/MarkerSelection/master/Data/Outputs/CSVs_and_Tables/Markers/All_hodge_regions/new_ALLReg_results_class_ITexpand_lfct15_minpct25_dup.csv")[,-1]
 
 paste("marker matches in data: ", length(intersect(unlist(data_working$Gene.Symbol), unlist(marker_data$gene))), "/",
       nrow(marker_data))
 
 # Get vector of unique cell types 
-(cell_types = marker_data$subclass %>% unique())
+(cell_types = marker_data$class %>% unique())
 # Organize markers into a list 
 marker_list = lapply(cell_types, function(cell_type){
-  return(marker_data %>% filter(subclass == cell_type) %>% pull(gene) %>% unlist())
+  return(marker_data %>% filter(class == cell_type) %>% pull(gene) %>% unlist())
 })
 names(marker_list) = cell_types
 
@@ -101,7 +82,6 @@ mgp_estimates_all = as.data.frame(estimations$estimates) %>%
   scale() %>%
   as.data.frame()
 mgp_estimates_all = mgp_estimates_all[, order(names(mgp_estimates_all))]
-names(mgp_estimates_all) = gsub("\\.", "/", names(mgp_estimates_all))
 
 # run MGP analysis within
 estimations =  mgpEstimate(
@@ -121,7 +101,6 @@ mgp_estimates_within = as.data.frame(estimations$estimates) %>%
   scale() %>%
   as.data.frame()
 mgp_estimates_within = mgp_estimates_within[, order(names(mgp_estimates_within))]
-names(mgp_estimates_within) = gsub("\\.", "/", names(mgp_estimates_within))
 
 cor_test = cor(t(mgp_estimates_all), t(mgp_estimates_within), method = "spearman")
 
@@ -170,7 +149,6 @@ estimations =  mgpEstimate(
 # merge cell type proportions with sample metadata
 mgp_estimates = as.data.frame(estimations$estimates) %>%
   rownames_to_column(var = "sample_id")
-names(mgp_estimates) = gsub("\\.", "/", names(mgp_estimates))
 mgp_df = data_meta %>%
   inner_join(data_sme, by = "UT.code") %>%
   inner_join(mgp_estimates, by = "sample_id") %>%
@@ -208,6 +186,7 @@ ggarrange(plotlist = plot_list)
 mgp_estimates_sme = mgp_estimates %>% 
   filter(sample_id %in% within_meta$sample_id) %>%
   arrange(sample_id) %>%
+  inner_join(mri, by = "sample_id") %>%
   column_to_rownames(var = "sample_id")
 
 data_sme = data_sme %>%
@@ -220,12 +199,12 @@ data_sme = data_sme %>%
 cor_matrix = cor(mgp_estimates_sme, data_sme, method = "spearman")
 
 
-pdf("keon_analyses/output/correlation-heatmap-subclass.pdf",width=10,height=8)
+pdf("keon_analyses/output/correlation-heatmap-class.pdf",width=8,height=6)
 heatmap.2(cor_matrix, scale = "none",
           col = bluered(100), trace = "none", density.info = "none",
-          cexCol = 0.9,
-          cexRow = 0.8,
-          margins = c(6,8),
+          cexCol = 1,
+          cexRow = 1,
+          margins = c(7,9),
           key.xlab = "Rho",
           lmat = rbind(4:3,2:1),
           lwid = c(1.5,4),
@@ -300,7 +279,7 @@ ctp_df = ctp_df %>%
     cell_type == "SST" ~ "Inh",
     cell_type == "L4 IT" ~ "Exc",
     cell_type == "L3/5 IT" ~ "Exc",
-  cell_type == "L2/3 IT" ~ "Exc")
+    cell_type == "L2/3 IT" ~ "Exc")
   ) %>% 
   relocate(class, .before = "cell_type") %>%
   unite(cell_type, c(class, cell_type), sep = "_", remove = T, na.rm = T)
@@ -322,7 +301,7 @@ mgp_df$sample_id = gsub("_","", mgp_df$sample_id)
 combined_df = inner_join(mgp_df, ctp_df, by = c("sample_id","cell_type"))
 
 #pdf("keon_analyses/output/bulk-vs-sc-comparison.pdf",width=15,height=9)
-  ggplot(combined_df, aes(x = percent, y = cell_proportion)) +
+ggplot(combined_df, aes(x = percent, y = cell_proportion)) +
   geom_smooth(method = "lm", se = F) + 
   geom_point() +
   ylab(paste(plot_celltypes[i], "bulk-MGP")) + xlab ("sc-CTP") +
